03532cea0a3fffa393f411a974dc8b1f6cf72c8f
kseny1992 <xen_andeeva@bk.ru>
2025-08-21 14:33:18 +0300

    добавлен чек добавлена музыка

--- reminder.py
+++ reminder.py
@@ -5,7 +5,12 @@ import datetime
 import time
 import  pygame #для работы со звуком
 
+t=None #глобальная
+music=False #переменная для отслеживания проигрывания музыки
 def set(): #установка
+
+    global t
+
     rem = sd.askstring("время напоминания", "введите время напоминания в формате ЧЧ:ММ (в 24 часовом формате)")
     if rem:
         try:
@@ -13,20 +18,54 @@ def set(): #установка
             minute=int(rem.split(":")[1]) #аналогично
             now = datetime.datetime.now()
             print(now)
-            dt = now.replace(hour=hour,minute=minute) #заменяем часы на новые часы и минуты заменяем на минуты
+            dt = now.replace(hour=hour,minute=minute,second=0,microsecond=0) #заменяем часы на новые часы и минуты заменяем на минуты
             #получим текущее время в котором изменены часы и минуты
             print(dt)
             t=dt.timestamp()#получить временнУю метку в млд секунд
             print(t)
+            label.config(text= f"Напоминание установлено на: {hour:02}:{minute:02}")
+        except ValueError:
+            mb.showerror("Ошибка","Неверный формат времени")
         except EXCEPTION as e:
             mb.showerror(("ошибка",f"произошла ошибка: {e}"))
+def check(): #
+    global t
+    if t:#если не пустая
+        now = time.time() #время которое сейчас
+        if now>=t: #ecли >= то вкл напоминание
+            play_snd()
+            t=None #обнуляем
+    window.after(10000, check) #вызываем функцию чек через 10 секунд автоматически (6 раз в минуту) - рекурсия
+
+
+def play_snd():
+    global music
+    music = True
+    pygame.mixer.init()
+    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.play()
+
+
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music=False
+    label.config(text="Установить новое напоминание")
 
 
 window = Tk()
 window.title("Напоминание")
-label = Label(text="установите напоминание")
+label = Label(text="Установите напоминание")
+font = ("Arial",14)
 label.pack(pady=10)
-set_button = Button(text="установить", command = set)
-set_button.pack()
+
+set_button = Button(text="Установить напоминание", command = set)
+set_button.pack(pady=10)
+
+stop_button = Button(text="Остановить музыку", command=stop_music)
+stop_button.pack(pady=5)
+
+check()
 
 window.mainloop()
